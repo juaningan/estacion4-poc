d862bd03c8cd21fd33b30b4f2e7b53787c06f535
Juanjo Presa <juanjop@gmail.com>
2017-03-16 17:13:40 +0100

    Change bash version to centos naming

--- controlrepo/site/profile/manifests/base.pp
+++ controlrepo/site/profile/manifests/base.pp
@@ -1,6 +1,6 @@
 class profile::base {
   package { 'bash' :
-    ensure => '4.3-14ubuntu1',
-    #ensure => '4.3-11ubuntu1',
+    ensure => '4.2.46-20.el7_2',
+    #ensure => '4.2.46-21.el7_3',
   }
 }
